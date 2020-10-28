e867f288e4f02d89321310fcd1f57ad54f2cee2a
Sampada-VS <samssvv1@gmail.com>
2020-10-29 04:07:48 +0530

    Adding welcome message

---
+++ src/com/bridgelabz/gamblingproblem/GamblerSimulator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.gamblingproblem;
+
+public class GamblerSimulator {
+	public static void main(String[] args) {
+		System.out.println("Welcome to gambling simulator program.");
+	}
+}
