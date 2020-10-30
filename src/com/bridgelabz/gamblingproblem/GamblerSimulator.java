ad64694f963ee8509ca9d688d8c2572fd75e772a
Sampada-VS <samssvv1@gmail.com>
2020-10-30 15:50:53 +0530

    Adding- make bet either win or lose

--- src/com/bridgelabz/gamblingproblem/GamblerSimulator.java
+++ src/com/bridgelabz/gamblingproblem/GamblerSimulator.java
@@ -3,6 +3,21 @@ package com.bridgelabz.gamblingproblem;
 public class GamblerSimulator {
 	public static final int STAKE=100;
 	public static final int BET=1;
+	public static int stake=STAKE;
+	
+	public static void winOrLose() {
+		int winLose=(int)((Math.random()*10)%2);
+		if (winLose == 1) {
+			System.out.println("Gambler won bet.");
+			stake=stake+1;
+		}
+		else {
+			System.out.println("Gambler lost bet.");
+			stake=stake-1;	
+		}
+		System.out.println("Stake :"+stake);
+	}
 	public static void main(String[] args) {
+		winOrLose();
 	}
 }
